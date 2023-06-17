50ce79c754ec336421f0f2f87f5adcfee4dc4826
senouci <snoussia47@gmail.com>
2023-06-17 17:38:48 +0100

    add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 
 function App() {
   return (
-    <div>
+    <div className="text-4xl text-gray-700 tracking-widest capitalize text-center p-20">
       welcom to the product feedback app
     </div>
   );
